6329d5e02c940745d6b97bfaba375dd22d10a253
Jordan Senko <jsenk347@mtroyal.ca>
2025-11-14 03:25:03 +0000

    just starting

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+// Common syscalls:
+// socket(), bind(), connect(), recv(), send(), accept()
