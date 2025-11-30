1e5fb7473661222e294522d93f13d00d4d84bbd5
agall087 <agall087@mtroyal.ca>
2025-11-30 20:33:22 +0000

    fixed file traversal security issue

--- client.c
+++ client.c
@@ -11,7 +11,8 @@ int main(int argc, char *argv[])
     // the "\r\n\r\n" sequence signals the end of the request header block.
     // client is only responsible for sending over bytes. server must parse message once recieved
     const char message[] =
-        "GET www/HTTPSlides.png HTTP/1.1\r\n"
+        //"GET www/HTTPSlides.png HTTP/1.1\r\n" the www should be handled on the server side ARR
+        "GET /HTTPSlides.png HTTP/1.1\r\n"
         "Host: 127.0.0.1:6767\r\n"
         "Connection: close\r\n"
         "\r\n";
